353d5545138bf3257a90a42d34ca539f9ef07ed5
Hasnat Shafin <sh.shafin20@gmail.com>
2023-03-03 02:08:05 +0600

    beecrowd 1017

---
+++ Beecrowd/1017.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x,y;
+    cin>>x>>y;
+    float a=x*y;
+    cout<<fixed<<setprecision(3);
+    cout<<a/12.0<<endl;
+    return 0;
+}

00f57e8da7f62a99105e42ba65beaff699cfb1dd
Hasnat Shafin <sh.shafin20@gmail.com>
2023-03-03 00:42:05 +0600

    beecrowd 1013 update

--- Beecrowd/1013.cpp
+++ Beecrowd/1013.cpp
@@ -1,10 +1,13 @@
 #include<bits/stdc++.h>                                                                                                                                          
     using namespace std;
     int main(){
-        int a,b,c,MaiorAB,s,MAX;
+        int a,b,c,greater;
         cin>>a>>b>>c;
-        MaiorAB=(a+b+abs(a-b))/2;
-        MAX=(MaiorAB+c+abs(MaiorAB-c))/2;
-        cout<< MAX<<" eh o maior"<<endl;
+        if(a>b)
+        greater=std::max(a,c);
+        else 
+        greater=std::max(b,c);
+        cout<<greater<<endl;
+        
         return 0;
     }

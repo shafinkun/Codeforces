758f38cb9cbfbf60d12482156f75ac2e4c958806
Hasnat Shafin <sh.shafin20@gmail.com>
2023-03-03 01:31:56 +0600

    beecrowd 1015

---
+++ Beecrowd/1015.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#include<math.h>
+using namespace std;
+int main()
+{
+    double x1,x2,y1,y2,diff;
+    cin>>x1>>y1;
+    cin>>x2>>y2;
+    double a=x2-x1;
+    double b=y2-y1;
+    diff=pow((pow(a,2)+pow(b,2)),0.5);
+    cout<<fixed<<setprecision(4);
+    cout<<diff<<endl;
+    return 0;
+}

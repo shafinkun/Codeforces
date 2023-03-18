838db518b0553776c331910a9c33795fd220f4d5
Hasnat Shafin <sh.shafin20@gmail.com>
2023-03-19 00:21:31 +0600

    codechef taxsaving

---
+++ CodeChef/TAXSAVING.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x,y,t;
+    cin>>t;
+    while(t--)
+    {
+        cin>>x>>y;
+        if(x>y)
+        {
+            cout<<x-y<<endl;
+        }
+    }
+    return 0;
+}

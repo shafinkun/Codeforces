ab31eae62ec7a476200296c6b875b9071b7a9030
Hasnat Shafin <sh.shafin20@gmail.com>
2023-03-18 23:32:42 +0600

    codechef squarts

---
+++ CodeChef/SQUATS.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t,a,c=15;
+    cin>>t;
+    while(t--)
+    {
+        cin>>a;
+        cout<<a*c<<endl;
+    }
+    return 0;
+}

888aa312ec8c2a35e8d426fcc0a9bbbabb285c4b
Hasnat Shafin <sh.shafin20@gmail.com>
2023-04-12 00:29:45 +0600

    intial commit on formation

---
+++ udacity/basic25.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    int a = 45;
+    float b = 45.323;
+    double c = 45.5468;
+    int aa = a + 9;
+    float bb = b + 9;
+    double cc = c + 9;
+    int aaa = aa + 9;
+    float bbb = bb + 9;
+    double ccc = cc + 9;
+    cout<<a<<setw(15)<<b<<setw(15)<<c<<"\n";
+    cout<<aa<<setw(15)<<bb<<setw(15)<<cc<<"\n";
+    cout<<aaa<<setw(15)<<bbb<<setw(15)<<ccc;
+    return 0;
+}

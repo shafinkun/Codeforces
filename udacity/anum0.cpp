553140b964085371f6f6280bd52f7101a49f12de
Hasnat Shafin <sh.shafin20@gmail.com>
2023-04-15 16:53:43 +0600

    initial commit

---
+++ udacity/anum0.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    enum month{Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec};
+    month Born;
+    cout<<Born<<"\n";
+    if(Born==Oct) cout<<"Pinch!";
+}

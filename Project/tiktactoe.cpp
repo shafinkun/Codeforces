1695576c5b3176c0db504521372a3f9c859bb2d2
Hasnat Shafin <sh.shafin20@gmail.com>
2023-04-16 02:00:50 +0600

    update commit

--- Project/tiktactoe.cpp
+++ Project/tiktactoe.cpp
@@ -1,89 +1,107 @@
-//This is my first project
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
-int row,column;
-char a[3][3]={{'1','2','3'},{'4','5','6'},{'7','8','9'}};
-char turn='X';
-bool draw=false;
-void display(){
+
+
+char board[3][3] = { {'1','2','3'},{'4','5','6'},{'7','8','9'} };
+char turn = 'X';
+int row, column;
+bool draw = false;
+void display_board() {
     system("cls");
-    cout<<"\t\tTick Tac Toe Game\n";
-    cout<<"Player1 [X]\nPlayer2 [O]\n\n";
-    cout<<"\t\t     |     |     \n";
-    cout<<"\t\t  "<<a[0][0]<<"  |  "<<a[0][1]<<"  |  "<<a[0][2]<<"  \n";
-    cout<<"\t\t_____|_____|_____\n";
-    cout<<"\t\t     |     |     \n";
-    cout<<"\t\t  "<<a[1][0]<<"  |  "<<a[1][1]<<"  |  "<<a[1][2]<<"  \n";
-    cout<<"\t\t_____|_____|_____\n";
-    cout<<"\t\t     |     |     \n";
-    cout<<"\t\t  "<<a[2][0]<<"  |  "<<a[2][1]<<"  |  "<<a[2][2]<<"  \n";
-    cout<<"\t\t     |     |     \n";
+    cout << endl;
+    cout << "\t\tTIC   TAC   TOE" << endl;
+    cout << endl;
+    cout << "\tPLAYER1[X]\n \tPLAYER2[O]" << endl;
+    cout << endl;
+    cout << "\t\t     |     |     " << endl;
+    cout << "\t\t  " << board[0][0] << "  |  " << board[0][1] << "  |  " << board[0][2] << endl;
+    cout << "\t\t_____|_____|_____" << endl;
+    cout << "\t\t     |     |     " << endl;
+    cout << "\t\t  " << board[1][0] << "  |  " << board[1][1] << "  |  " << board[1][2] << endl;
+    cout << "\t\t_____|_____|_____" << endl;
+    cout << "\t\t     |     |     " << endl;
+    cout << "\t\t  " << board[2][0] << "  |  " << board[2][1] << "  |  " << board[2][2] << endl;
+    cout << "\t\t     |     |     " << endl;
+    cout << endl;
 }
-void player_turn(){
-    int choice;
-    if(turn=='X')
-    cout<<"\n\tPlayer1's[X] Turn: ";
-    if(turn=='O')
-    cout<<"\n\tPlayer1's[O] Turn: ";
-    cin>>choice;
-    switch(choice){
-        case 1: row=0;column=0; break;
-        case 2: row=0;column=1; break;
-        case 3: row=0;column=2; break;
-        case 4: row=1;column=0; break;
-        case 5: row=1;column=1; break;
-        case 6: row=1;column=2; break;
-        case 7: row=2;column=0; break;
-        case 8: row=2;column=1; break;
-        case 9: row=2;column=2; break;
-        default:
-            cout<<"Invalid Choice";
-            break;
+void player_turn() {
+    int choise;
+
+    if (turn == 'X') {
+        cout << "\tPLAYER 1[X] TURN :";
+        cin >> choise;
     }
-    if(turn=='X' && a[row][column]!='X' && a[row][column]!='O')
-        {
-            a[row][column]='X';
-            turn='O';
-        }
-    else if(turn=='O' && a[row][column]!='X' && a[row][column]!='O')
-        {
-            a[row][column]='O';
-            turn='X';
-        } 
-    else 
+    if (turn == 'O') {
+        cout << "\tPLAYER 2[O] TURN :";
+        cin >> choise;
+    }
+    switch (choise)
     {
-        cout<<"Box already taken\n";
+    case 1: row = 0; column = 0; break;
+    case 2: row = 0; column = 1; break;
+    case 3: row = 0; column = 2; break;
+    case 4: row = 1; column = 0; break;
+    case 5: row = 1; column = 1; break;
+    case 6: row = 1; column = 2; break;
+    case 7: row = 2; column = 0; break;
+    case 8: row = 2; column = 1; break;
+    case 9: row = 2; column = 2; break;
+    default:
+        cout << "invalid choise..!!" << endl;
+        break;
+    }
+    if (turn == 'X' && board[row][column] != 'X' && board[row][column] != 'O') {
+        board[row][column] = 'X';
+        turn = 'O';
+    }
+    else if (turn == 'O' && board[row][column] != 'X' && board[row][column] != 'O') {
+        board[row][column] = 'O';
+        turn = 'X';
+    }
+    else {
+        cout << "Box Taken!\n Please Try Again!" << endl;
         player_turn();
     }
-    display();
+    display_board();
 }
-bool gameover(){
-    //check win;
-    for(int i=0;i<3;i++)
-    if(a[i][0]==a[i][i] && a[i][0]==a[i][2] || a[0][i]==a[1][i]&&a[0][i]==a[2][i])
-    return false;
 
-    if(a[0][0]==a[1][1] && a[0][0]==a[2][2] || a[0][2]==a[1][1]&&a[0][0]==a[2][0])
-    return false;
-    for(int i=0;i<3;i++)
-    for(int j=0;j<3;j++)
-    if(a[i][j]!='X' && a[i][j]!='O')
-    return true;
+bool gameover() {
+    // check win
+    for (int i = 0; i < 3; i++) {
+        if (board[i][0] == board[i][1] && board[i][0] == board[i][2] || board[0][i] == board[1][i] && board[0][i] == board[2][i])
+        return false;
+    }
+    if (board[0][0] == board[1][1] && board[0][0] == board[2][2] || board[0][2] == board[1][1] && board[0][2] == board[2][0]) {
+        return false;
+    }
 
-    draw=true;
+    //game playing
+    for (int i = 0; i < 3; i++)
+    for (int j = 0; j < 3; j++)
+        if (board[i][j] != 'X' && board[i][j] != 'O') {
+            return true;
+        }
+    //game draw
+    draw = true;
     return false;
 }
-int main()
-{
-    while(gameover()){
-    int choice;
-    display();
-    player_turn();
-    };
-    if(turn=='X' && draw==false)
-    cout<<"Player2 [O] Wins!!! Congratulations\n";
-    if(turn=='O' && draw==false)
-    cout<<"Player1 [X] Wins!!! Congratulations\n";
-    else
-    cout<<"Game Drawn!";
+
+int main() {
+    while (gameover()) {
+        display_board();
+        player_turn();
+        gameover();
+        
+    }
+    if (turn == 'X' && draw == false) {
+        cout << "player 2[O] WINS!!!" << endl;
+    }
+    else if (turn == 'O'  && draw == false) {
+        cout << "player 1[X] WINS!!!" << endl;
+    }
+    else {
+        cout << "GAME DRAW!!" << endl;
+    }
+
+
 }

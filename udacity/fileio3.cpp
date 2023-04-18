3f7c11f664e186ec1ffd9eeab51ae894851cffe0
Hasnat Shafin <sh.shafin20@gmail.com>
2023-04-18 16:15:53 +0600

    initial commit

---
+++ udacity/fileio3.cpp
@@ -0,0 +1,10 @@
+//here we'll transfer a writing in a string to a file
+#include<bits/stdc++.h>
+#include<fstream>
+using namespace std;
+int main()
+{
+    string st="my github username is: hasnatshafin01";
+    ofstream out("anothersample.txt");
+    out<<st;
+}

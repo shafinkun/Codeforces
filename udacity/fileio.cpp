2ad0f7cf41df548110c1ce4759d6de440cb72e92
Hasnat Shafin <sh.shafin20@gmail.com>
2023-04-18 14:10:32 +0600

    update commit

--- udacity/fileio.cpp
+++ udacity/fileio.cpp
@@ -1,7 +1,17 @@
 #include<bits/stdc++.h>
 #include<fstream>
-#include<strings.h>
+using namespace std;
 int main()
 {
-    string st="This is Shafin";
+    string st="This is Shafin Ahmed!";
+    //Declare a file
+    ofstream out("sample1.txt");
+    out<<st;
+    string st2;
+    ifstream in("sample2.txt");
+    in>>st2;
+    cout<<st2<<endl;
+    getline(in,st2);
+    cout<<st2;
+    return 0;
 }

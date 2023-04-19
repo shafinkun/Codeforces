566012fc6ba38ffc46c1ed11bc6e6ad03d6fbac6
Hasnat Shafin <sh.shafin20@gmail.com>
2023-04-19 12:42:46 +0600

    Update fileIO

---
+++ udacity/fileio5.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#include<fstream>
+using namespace std;
+int main()
+{
+    string st;
+    ifstream hin("anothersample.txt");
+    getline(hin,st);
+    cout<<st<<endl;
+    getline(hin,st);
+    cout<<st<<endl;
+    getline(hin,st);
+    cout<<st<<endl;
+    getline(hin,st);
+    cout<<st;
+}

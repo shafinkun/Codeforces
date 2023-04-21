4d6d8af7cb3ebabdfe0517e780b7d73f45b4df98
Hasnat Shafin <sh.shafin20@gmail.com>
2023-04-21 13:09:13 +0600

    update commit

--- CodeChef/EZSPEAK.cpp
+++ CodeChef/EZSPEAK.cpp
@@ -1,35 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
-bool check(char a){
-    if(a!='a' && a!='e' && a!='i' &&a!='o' && a!='u'){return 1;}
-    else {return 0;}
+bool check(char wo)
+{
+    if(wo=='a' || wo=='e' || wo=='i' || wo=='o' || wo=='u'){return 0;}
+    else {return 1;}
 }
 int main()
 {
-    int j,t;
-    char word[j];
+    int j,t,count=0;
+    string word;
     cin>>t;
-    while(t)
+    while(t--)
     {
     cin>>j;
-    cin>>word;          
-    if(j<=3){cout<<"YES\n";}
-    else if(j>=4)
+    cin>>word;
+    if(j<4){cout<<"YES\n";}
+    else
     {   
-        int count=0;
         for(int i=0;i<j;i++)
         {
         if(check(word[i]))
         {
             count++;
-            if(count>=4){cout<<"NO\n";
-            break;}
+            if(count<4){cout<<"YES\n";
+                count=0;
+                break;}
         }
         else{count=0;}
         }
-    if(count<4){cout<<"YES\n";}
-    t--;
+    if(count>=4){cout<<"NO\n";
+    count=0;}
     }
     }
-    return 0;
 }
+

26ef966b449e3c4fbaccf6c99a7fb33f05e6d1fe
Hasnat Shafin <sh.shafin20@gmail.com>
2023-04-23 00:55:47 +0600

    commiting NSTU lab's code

--- Misc Problem/mashuqstrinagle.cpp
+++ Misc Problem/mashuqstrinagle.cpp
@@ -5,5 +5,16 @@ int main()
     int n;
     cout<<"Value of a of the triangle: ";
     cin>>n;
-    for(int i=)
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=n;j>i;j--)
+        {cout<<" ";}
+            for(int k=2*i;k>1;k--)
+            {
+                if(i==n)cout<<1;
+                else if(k==2*i||k==2)cout<<1;
+                else cout<<0;
+            }
+        cout<<endl;
+    }
 }

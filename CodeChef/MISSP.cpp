34070312f9cc4dba2b69cb882b94e701d20b088c
Hasnat Shafin <sh.shafin20@gmail.com>
2023-04-24 11:50:01 +0600

    submitted code

--- CodeChef/MISSP.cpp
+++ CodeChef/MISSP.cpp
@@ -2,19 +2,17 @@
 using namespace std;
 int main()
 {
-    int t,n,count=0;
-    int num[n];
+    int t,n,count=0,num;
     cin>>t;
     cin>>n;
-    while(t)
+    while(t--)
     {   
         int m=n;
         for(int i=0;i<m;i++)
         {
-            cin>>num[i];
-            if(num[i] % 2 != 0) count++;
-        }
-        t--;    
+            cin>>num;
+            if(num % 2 != 0) count++;
+        }  
     }
     cout<<count;
     return 0;

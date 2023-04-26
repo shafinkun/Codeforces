8b9456ff915c3a001de85c0a55256a57ad7006b3
Hasnat Shafin <sh.shafin20@gmail.com>
2023-04-26 16:14:17 +0600

    initial commit

---
+++ CodeChef/ADJSUMPAR.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t,a,i,b[i];
+    cin>>t;
+    while(t--){
+        cin>>a;
+        
+        if(a%2!=0)
+    }
+
+    return 0;
+}

ed89c5890bcc64a0c74dbd6d2bed6a247a501dd7
Hasnat Shafin <sh.shafin20@gmail.com>
2023-04-29 22:46:00 +0600

    update commit

--- CodeChef/SNAKPROC.cpp
+++ CodeChef/SNAKPROC.cpp
@@ -1,23 +1,46 @@
+/*6
+18
+..H..T...HTH....T.
+3
+...
+10
+H..H..T..T
+2
+HT
+11
+.T...H..H.T
+7
+H..T..H
+*/
+
+
 #include<bits/stdc++.h>
-using namespace std;
+using namespace std;                    
 int main()
 {
-    int t,n,a;
-    string s;
-    char sr[500];
+    int t,n,m=0;
+    string s,sr;
     cin>>t;
     while(t--)
     {
-        int a=0;
         cin>>n;
         cin>>s;
         for(int i=0;i<n;i++)
         {
-            if(s[i]=='.') continue;
-            sr[a]=s[i];
-            a++;
-        } 
-        cout<<sr;
+            if(s[i]!='.'){sr+=s[i];}
+        }
+        int b=sr.length();
+        if(b==0) cout<<"Valid\n";
+        else if(b%2==0)
+        {
+            for(int i=0,j=1;i<b,j<b;i=i+2,j=j+2){
+                    if(j==b-1) { break;}
+                    if((sr[i]==sr[i+2]=='H') && (sr[j]==sr[j+2]=='T')) {m=1;}}
+        }
+        else if(b%2!=0) cout<<"Invalid\n";    
+        if(m==1) cout<<"Valid\n";
+        else if(m==0) ;cout<<"Invalid\n";
     }
+
     return 0;
 }
--- Misc Problem/mashuqstriangle2.cpp
+++ Misc Problem/mashuqstriangle2.cpp
@@ -1,20 +1,19 @@
 #include<bits/stdc++.h>
-using namespace std;
 int main()
 {
     int n;
-    cout<<"Value of a of the triangle: ";
-    cin>>n;
+    std::cout<<"Value of a of the triangle: ";
+    std::cin>>n;
     for(int i=1;i<=n;i++)
     {
         for(int j=n;j>i;j--)
-        {cout<<" ";}
+        {std::cout<<" ";}
             for(int k=2*i;k>1;k--)
             {
-                if(i==n)cout<<0;
-                else if(k==2*i||k==2)cout<<0;
-                else cout<<1;
+                if(i==n)std::cout<<0;
+                else if(k==2*i||k==2)std::cout<<0;
+                else std::cout<<1;
             }
-        cout<<endl;
+        std::cout<<std::endl;
     }
 }

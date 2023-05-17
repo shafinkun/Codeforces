543dc4b2bd19eb49212ee72e3a5bee61d8a322d2
Hasnat Shafin <sh.shafin20@gmail.com>
2023-05-17 23:45:06 +0600

    Initial commit

---
+++ Misc Problem/mid.cpp
@@ -0,0 +1,10 @@
+//to know about time delay
+#include<iostream>
+using namespace std;
+int main()
+{
+    int sum=0;
+    for(int n=0;n<1000000000;n++);
+    cout<<sum;
+    cout<<"Dekh shala koto boro code!";
+}

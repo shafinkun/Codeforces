5239837975f930bda9832da593f8d17a0056060b
Hasnat Shafin <sh.shafin20@gmail.com>
2023-05-20 04:20:45 +0600

    submitted commit

---
+++ CodeChef/MUFFINS3.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a = 0;
+        cin >> a;
+        int c = a / 2 + 1;
+        cout << c << endl;
+    }
+    return 0;
+}

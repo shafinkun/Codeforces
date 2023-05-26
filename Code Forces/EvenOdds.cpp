540a455670a1b052b66dec73fcd3b5d4f7c685f5
Hasnat Shafin <sh.shafin20@gmail.com>
2023-05-26 20:07:47 +0600

    Initial Commit

--- Code Forces/EvenOdds.cpp
+++ Code Forces/EvenOdds.cpp
@@ -1,10 +1,83 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    int n,k;
-    cin>>n>>k;
-    char s[n];
-    
+    long long n, k, num = 0;
+    cin >> n >> k;
+    if (k > n)
+        return 0;
+    if (n % 2 == 0)
+    {
+        if (k <= n / 2)
+        {
+            num = 1;
+            if (k == 1)
+            {
+                cout << num;
+            }
+            else
+            {
+                for (int i = 1; i < k; i++)
+                {
+                    num = num + 2;
+                }
+                cout << num;
+            }
+        }
+        else
+        {
+            num = 2;
+            k = k - (n / 2);
+            if (k == 1)
+            {
+                cout << num;
+            }
+            else
+            {
+                for (int i = 1; i < k; i++)
+                {
+                    num = num + 2;
+                }
+                cout << num;
+            }
+        }
+    }
+    else
+    {
+        if (k <= (n / 2) + 1)
+        {
+            num = 1;
+            if (k == 1)
+            {
+                cout << num;
+            }
+            else
+            {
+                for (int i = 1; i < k; i++)
+                {
+                    num = num + 2;
+                }
+                cout << num;
+            }
+        }
+        else
+        {
+            num = 2;
+            k = k - (n / 2) - 1;
+            if (k == 1)
+            {
+                cout << num;
+                return 0;
+            }
+            else
+            {
+                for (int i = 1; i < k; i++)
+                {
+                    num = num + 2;
+                }
+                cout << num;
+            }
+        }
+    }
     return 0;
 }

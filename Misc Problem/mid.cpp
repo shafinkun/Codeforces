27506add42727807ce0eeadad3b379ffb7f486c1
Hasnat Shafin <sh.shafin20@gmail.com>
2023-06-02 23:45:25 +0600

    Changed the output

--- Misc Problem/mid.cpp
+++ Misc Problem/mid.cpp
@@ -1,10 +1,10 @@
 //to know about time delay
 #include<iostream>
-using namespace std;
 int main()
 {
     int sum=0;
     for(int n=0;n<1000000000;n++);
-    cout<<sum;
-    cout<<"Dekh shala koto boro code!";
+    std::cout<<sum<<std::endl;
+    std::cout<<"Dekh koto delay!";
+    return 0;
 }

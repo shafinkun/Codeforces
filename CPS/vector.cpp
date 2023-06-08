89dc37833bfacdb736f2b2980b94c6fc60f1a28c
Hasnat Shafin <sh.shafin20@gmail.com>
2023-06-08 20:45:00 +0600

    Initial commit

---
+++ CPS/vector.cpp
@@ -0,0 +1,15 @@
+//First time using vector
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector <int> v;
+    v.push_back (2);
+    v.push_back (3);
+    v.push_back (3);
+    v.push_back (4);
+    for(int i=0;i<v.size();i++)
+    {
+        cout<<v[i];
+    }
+}

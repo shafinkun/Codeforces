890814e63c95fcfb150e85e5f33c87c53de2c34c
Hasnat Shafin <sh.shafin20@gmail.com>
2023-06-08 23:14:28 +0600

    Updated commit

--- Code Forces/WordCapitalization.cpp
+++ Code Forces/WordCapitalization.cpp
@@ -1,10 +1,10 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
     string s;
-    cin>>s;
-    s[0] = (char) toupper(s[0]);
-    cout<<s; 
+    cin >> s;
+    s[0] = toupper(s[0]);
+    cout << s;
     return 0;
 }

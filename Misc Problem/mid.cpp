1b0228d8bdb54e6c294db323890be1544e011517
Shahriar Hasnat Shafin Ahmed <126312684+shafinkun@users.noreply.github.com>
2023-06-29 07:32:30 +0600

    Added Bits std

--- Misc Problem/mid.cpp
+++ Misc Problem/mid.cpp
@@ -1,10 +1,14 @@
 //to know about time delay
-#include<iostream>
+#include<bits/stdc++.h>
+
 int main()
 {
     int sum=0;
     for(int n=0;n<1000000000;n++);
+
     std::cout<<sum<<std::endl;
     std::cout<<"Dekh koto delay!";
+
+
     return 0;
 }

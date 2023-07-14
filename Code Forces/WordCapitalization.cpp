036b5b9fd1e06cd19dbc857f0e98acbd145cdf85
Hasnat Shafin <sh.shafin20@gmail.com>
2023-07-14 23:26:20 +0600

    Updated Code

--- Code Forces/WordCapitalization.cpp
+++ Code Forces/WordCapitalization.cpp
@@ -1,7 +1,14 @@
 #include <bits/stdc++.h>
+#define endl '\n';
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
 using namespace std;
 int main()
 {
+    optimize();
+    
     string s;
     cin >> s;
     s[0] = toupper(s[0]);

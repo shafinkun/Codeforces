83493345909239366bdc15a85f075b9afd44e112
Shahriar Hasnat Shafin Ahmed <126312684+shafinkun@users.noreply.github.com>
2023-07-27 03:32:35 +0600

    Update pattern3.cpp

--- Misc Problem/pattern3.cpp
+++ Misc Problem/pattern3.cpp
@@ -1,19 +1,20 @@
 //reverse triangle
-#include<iostream>
-#include<conio.h>
+#include<bits/stdc++.h>
+
 using namespace std;
 int main()
 {
     int n;
     cout<<"Input the value of n:";
-    cin>>n;
+    cin >> n;
 
-    for(int i=1;i<=n;i++)
+    for(int i = 1;i <= n; i++)
     {
-        for(int k=n;k>=i;k--){
-            cout<<"*";}
-            cout<<endl;
+        for(int k = n; k >= i; k--){
+            cout << "*";}
+            cout << endl;
     }
+
     return 0;
-    getch;
+
 }

317011804385d41468353682421e137b46f68507
Shahriar Hasnat Shafin Ahmed <126312684+shafinkun@users.noreply.github.com>
2023-08-01 15:53:29 +0600

    Update pattern10.cpp

--- Misc Problem/pattern10.cpp
+++ Misc Problem/pattern10.cpp
@@ -1,19 +1,19 @@
 //inverted pattern
-#include<iostream>
-#include<conio.h>
+#include<bits/stdc++.h>
+
 using namespace std;
 int main()
 {
     int n;
-    cin>>n;
-    for(int i=1;i<=n;i++)
+    cin >> n;
+    for(int i=1 ; i <= n ; i++)
     {
-        for(int k=1;k<=n+1-i;k++){      //there is an another way to do
+        for(int k = 1 ; k <= n+1-i ; k++)       {      //there is an another way to do
                                         //you know how it is
             cout<<k;
         }
         cout<<endl;
     }
+
     return 0;
-    getch;
 }

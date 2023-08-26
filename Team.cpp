6a40093d878d7f55c8a2692c8a3565610b2f885f
Shafin Ahemd <sh.shafin20@gmail.com>
2023-08-27 01:46:53 +0600

    Initial Commit

--- Team.cpp
+++ Team.cpp
@@ -4,19 +4,10 @@ int main()
 {
     int t;
     cin >> t;
-    int count = 0;
-    while (t--)
-    {
-        int a[3];
-        for (int i = 0; i < 3; i++)
-        {
-            cin >> a[i];
-        }
-        if (a[0] && a[1] || a[0] && a[2] || a[2] && a[1] || a[0] && a[1] && a[2])
-        {
-            count++;
-        }
-    }
-    cout << count;
+    if (t > 0)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+
     return 0;
 }

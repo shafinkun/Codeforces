60c3edb0073201aa54125f12be31f19e432b22c7
Shafin Ahemd <sh.shafin20@gmail.com>
2023-08-29 18:30:11 +0600

    Accepted Code

--- RaisingBacteria.cpp
+++ RaisingBacteria.cpp
@@ -1,43 +1,81 @@
 #include <bits/stdc++.h>
-#define endl '\n';
-#define optimize()                \
-    ios_base::sync_with_stdio(0); \
-    cin.tie(0);                   \
-    cout.tie(0);
 using namespace std;
+
+#define optimize()                    \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+#define ll long long
+#define ull unsigned long long
+#define pii pair<int, int>
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector<vector<int>>
+#define vvl vector<vector<ll>>
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+
+// Shortcuts for common functions
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+#define forn(i, n) for (int i = 0; i < (int)(n); ++i)
+#define forr(i, a, b) for (int i = (int)(a); i <= (int)(b); ++i)
+#define ford(i, a, b) for (int i = (int)(a); i >= (int)(b); --i)
+
+// Binary Exponentiation
+ll power(ll x, ll y)
+{
+    ll res = 1;
+    while (y > 0)
+    {
+        if (y & 1)
+            res = res * x;
+        x = x * x;
+        y >>= 1;
+    }
+    return res;
+}
+
+// Greatest Common Divisor
+ll gcd(ll a, ll b)
+{
+    return (b == 0) ? a : gcd(b, a % b);
+}
+
+// Debugging Macros
+#ifdef DEBUG
+#define dbg(x) cerr << #x << " : " << x << endl
+#else
+#define dbg(x)
+#endif
+
+void solve(int x){
+    int ans = 0;
+    while(x){
+        if(x % 2 == 1){
+            ans++;
+            x--;
+        }
+        else{
+            x /= 2;
+        }
+    }
+    cout << ans << endl;  
+}
+
 int main()
 {
     optimize();
 
     int n;
     cin >> n;
-    
-    if(n<=2){
-        cout << 1 << endl;
-    }
-    
-    else {
-        if(n % 2 == 0 && n % 4 == 0 && n % 3 != 0 && n % 10 != 0)
-        {
-            cout << 1 << endl;
-        }
-        else{
-            int count = 0;
-            if(n % 2 != 0){
-                n--;
-                count++;
-            }
-            while(n = n/ 2)
-            {
-                if(n % 2 != 0){
-                    n--;
-                    count++;
-                }
-            }
 
-            cout << count << endl;
-        }
-    }
+    solve(n);
 
     return 0;
 }

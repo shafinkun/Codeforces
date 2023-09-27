6bbda04d9b33fbaf44b75754ae94df527cfbe1c4
Shafin Ahemd <sh.shafin20@gmail.com>
2023-09-27 22:31:08 +0600

    Accepted Code

---
+++ 1878C.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define optimize()                    \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+#define ll long long
+#define ull unsigned long long
+#define pii pair<int, int>
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector<vector<int>>
+#define vvl vector<vector<ll>>
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+
+// Shortcuts for common functions
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+#define forn(i, n) for (int i = 0; i < (int)(n); ++i)
+#define forr(i, a, b) for (int i = (int)(a); i <= (int)(b); ++i)
+#define ford(i, a, b) for (int i = (int)(a); i >= (int)(b); --i)
+
+// Binary Exponentiation
+ll power(ll x, ll y)
+{
+    ll res = 1;
+    while (y > 0)
+    {
+        if (y & 1)
+            res = res * x;
+        x = x * x;
+        y >>= 1;
+    }
+    return res;
+}
+
+// Greatest Common Divisor
+ll gcd(ll a, ll b)
+{
+    return (b == 0) ? a : gcd(b, a % b);
+}
+
+// Debugging Macros
+#ifdef DEBUG
+#define dbg(x) cerr << #x << " : " << x << endl
+#else
+#define dbg(x)
+#endif
+
+void solve()
+{
+    ll a, b, c;
+    cin >> a >> b >> c;
+
+    if (2 * c >= b * (b + 1) && 2 * c <= (a * (a + 1) - (a - b) * (a - b + 1)))
+        yes;
+    else
+        no;
+}
+
+int main()
+{
+    optimize();
+
+    short t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}

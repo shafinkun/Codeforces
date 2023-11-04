84a04916a827a3a27c22924a5915a69a01198a96
Shahriar Hasnat Shafin Ahmed <126312684+shafinkun@users.noreply.github.com>
2023-11-04 22:37:16 +0600

    Update code using mathematical logic

--- 996A.cpp
+++ 996A.cpp
@@ -165,35 +165,15 @@ void solve()
 {
     int n;
     cin >> n;
-    int cnt = 0;
-    while (n > 0)
-    {
-        if (n >= 100)
-        {
-            n -= 100;
-            cnt++;
-        }
-        else if (n >= 20)
-        {
-            n -= 20;
-            cnt++;
-        }
-        else if (n >= 10)
-        {
-            n -= 10;
-            cnt++;
-        }
-        else if (n >= 5)
-        {
-            n -= 5;
-            cnt++;
-        }
-        else
-        {
-            n -= 1;
-            cnt++;
-        }
-    }
+    int cnt = n / 100;
+    n %= 100;
+    cnt += n / 20;
+    n %= 20;
+    cnt += n / 10;
+    n %= 10;
+    cnt += n / 5;
+    n %= 5;
+    cnt += n;
     cout << cnt << endl;
 }
 int main()

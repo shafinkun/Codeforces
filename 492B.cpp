110e4e7cae5a93d9dd2de20eba73234cb1f5b462
Shahriar Hasnat Shafin Ahmed <126312684+shafinkun@users.noreply.github.com>
2023-12-12 08:01:48 +0600

    Hint: Check the max diffrence , start and end values

--- 492B.cpp
+++ 492B.cpp
@@ -1,5 +1,8 @@
 /*____________________In The Name Of Allah________________*/
 /*_____________________Solved By shafinkun________________*/
+// Problem link : https://codeforces.com/problemset/problem/492/B
+
+
 #include <bits/stdc++.h>
 using namespace std;
 #define optimize()                \
@@ -8,27 +11,29 @@ using namespace std;
     cout.tie(0);
 #define endl '\n'
 
+
 void solve()
 {
     int n, l;
     cin >> n >> l;
-    vector<int> v(n + 1);
-    for (int i = 1; i <= n; i++)
-    {
-        cin >> v[i];
-    }
-    int mx = INT_MIN;
+    vector<int> v(n);
+    for (auto &x : v)
+        cin >> x;
+
     sort(v.begin(), v.end());
- 
-    for (int i = 1; i <= n; i++)
+    int mx = 0;
+
+    for (int i = 1; i < n; i++)
     {
-        int dis = abs(v[i] - v[i - 1]);
-        mx = max(mx, dis);
+        int dif = v[i] - v[i - 1];
+        mx = max(mx, dif);
     }
-    
-    double d = 1.0 * mx / 2, fin_d = abs(0 - v[1]), fin_dd = abs(l - v[n]);
-    
-    cout << fixed << setprecision(10) << max(d, max(fin_d, fin_dd)) << endl;
+
+    double d1 = mx / 2.0, d2 = v[0], d3 = (l - v[n - 1]);
+
+    // cerr << d1 << " " << d2 << " " << d3;
+
+    cout << fixed << setprecision(10) << max(d1, max(d2, d3)) << endl;
 }
 int main()
 {

2279fffba51113b41e21b13ff44761897bf1d063
Shahriar Hasnat Shafin Ahmed <126312684+shafinkun@users.noreply.github.com>
2023-12-12 23:58:16 +0600

    Initial Commit

---
+++ 581A.cpp
@@ -0,0 +1,32 @@
+/*____________________In The Name Of Allah________________*/
+/*_____________________Solved By shafinkun________________*/
+// Problem link : https://codeforces.com/problemset/problem/581/A
+
+#include <bits/stdc++.h>
+using namespace std;
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define endl '\n'
+
+void solve()
+{
+    int a, b;
+    cin >> a >> b;
+
+    int c = abs(a - b) / 2;
+
+    cout << min(a, b) << " " << c << endl;
+    return;
+}
+int main()
+{
+    optimize();
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}

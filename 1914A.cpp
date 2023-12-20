e8dd572e0ed4e256d9a1db96286806472c2bf77a
Shahriar Hasnat Shafin Ahmed <126312684+shafinkun@users.noreply.github.com>
2023-12-20 16:50:43 +0600

    Initial Commit

---
+++ 1914A.cpp
@@ -0,0 +1,45 @@
+/*_______________In the name of Allah_____________*/
+/*________________Solved By shafinkun_____________*/
+// Problem link : https://codeforces.com/contest/1914/problem/A
+
+#include <bits/stdc++.h>
+using namespace std;
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define endl '\n'
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    string s;
+    cin >> s;
+
+    unordered_map<char, int> mp;
+    for(auto x : s)
+        mp[x]++;
+
+    int cnt = 0;
+    for(auto x : mp)
+    {
+       if(x.second >= x.first - 'A' + 1)
+            cnt++;
+    }
+
+    cout << cnt << endl;
+    return;
+}
+
+int main()
+{
+    optimize();
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}

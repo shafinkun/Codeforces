30e0eba0d44f482553784e1e86f2211f24fc76fb
Shahriar Hasnat Shafin Ahmed <126312684+shafinkun@users.noreply.github.com>
2024-03-07 23:31:12 +0600

    Updated gitignore

---
+++ 1904A.cpp
@@ -0,0 +1,42 @@
+/*__________________In the name of Allah________________*/
+/*___________________Solved By shafinkun________________*/
+// Problem link : https://codeforces.com/contest/1805/problem/B
+
+#include <bits/stdc++.h>
+using namespace std;
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define endl '\n'
+#define ll long long
+#define yes puts("YES")
+#define no puts("NO")
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    int knightX, knightY, queenX, queenY, kingX, kingY;
+    cin >> knightX >> knightY;
+    cin >> queenX >> queenY;
+    cin >> kingX >> kingY;
+
+    
+   
+
+    return;
+}
+
+int main()
+{
+    optimize();
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}

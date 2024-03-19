8b9bb2409b48c90836c273e79a2d09c5960b3a69
Shahriar Hasnat Shafin Ahmed <126312684+shafinkun@users.noreply.github.com>
2024-03-19 21:31:02 +0600

    Easy peasy lemon squeezy problem

---
+++ 1370A.cpp
@@ -0,0 +1,37 @@
+/*__________________In the name of Allah________________*/
+/*___________________Solved By shafinkun________________*/
+// Problem link : https://codeforces.com/problemset/problem/1370/A
+
+#include <bits/stdc++.h>
+using namespace std;
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define endl '\n'
+#define ll long long
+#define yes puts("YES")
+#define no puts("NO")
+
+void solve(int a)
+{
+    int n;
+    cin >> n;
+
+    int ans = n / 2;
+    cout << ans << endl;
+
+    return;
+}
+
+int main()
+{
+    optimize();
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve(t);
+    }
+    return 0;
+}

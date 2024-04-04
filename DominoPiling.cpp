09eadb88ec2d2866ae7dd9465cb3cf8607710f17
Shahriar Hasnat Shafin Ahmed <126312684+shafinkun@users.noreply.github.com>
2024-04-04 14:34:52 +0600

    Update DominoPiling.cpp

--- DominoPiling.cpp
+++ DominoPiling.cpp
@@ -2,10 +2,13 @@
 using namespace std;
 int main()
 {
-    int m,n,r;
-    cin>>m>>n;
-    if(m%2==0)r=n*(m/2);
-    else r=n*(m/2)+n/2;
-    cout<<r;
+    int m, n, r;
+    cin >> m >> n;
+
+    if(m & 1) r = n * (m / 2) + n / 2;
+    else r = n * (m / 2);
+
+    cout << r << endl;
+
     return 0;
 }

5d77fa26c6a98f7ef58ff4d3841cf870cef96e08
shafinkun <sh.shafin20@gmail.com>
2024-06-18 18:08:25 +0600

    X >= 2 , so ans is 2 (! for n = 3)

---
+++ 1985B.cpp
@@ -0,0 +1,37 @@
+/*___________________In the name of Allah________________*/
+/*___________________Solved By shafinkun________________*/
+// Problem link : https://codeforces.com/contest/1985/problem/B
+
+#include <bits/stdc++.h>
+using namespace std;
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define endl '\n'
+#define ll long long
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+const int MOD = 1e9 + 7;
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    cout << ((n == 3) ? 3 : 2) << endl;
+
+    return;
+}
+
+int main()
+{
+    optimize();
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}

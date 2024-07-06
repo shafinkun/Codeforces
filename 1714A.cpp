00136fe314fda7d4077aa687c9da53ae72009363
shafinkun <sh.shafin20@gmail.com>
2024-07-06 13:58:10 +0600

    calculate in minutes, if h < H: add 24hrs

--- 1714A.cpp
+++ 1714A.cpp
@@ -9,7 +9,7 @@ using namespace std;
     cin.tie(0);                   \
     cout.tie(0);
 #define endl '\n'
-#define ll long long
+#define int long long
 #define NO cout << "NO" << endl
 #define YES cout << "YES" << endl
 void solve()
@@ -17,14 +17,14 @@ void solve()
     int n, h, m;
     cin >> n >> h >> m;
 
-    ll time = h * 60 + m;
+    int time = h * 60 + m;
 
-    ll ans = 1440;
+    int ans = 1440;
 
     while (n--)
     {
         cin >> h >> m;
-        ll temp = h * 60 + m;
+        int temp = h * 60 + m;
 
         if (temp < time)
             temp += 1440;
@@ -37,7 +37,7 @@ void solve()
     return;
 }
 
-int main()
+signed main()
 {
     optimize();
     // freopen("input.txt", "r", stdin);

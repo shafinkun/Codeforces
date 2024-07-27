9a81428a1efe19df02dc83d49593daff3dccec3e
shafinkun <sh.shafin20@gmail.com>
2024-07-27 18:54:15 +0600

    cow = n/2; left legs=n%4; chicks = n/2

---
+++ Solutions/1996A.cpp
@@ -0,0 +1,43 @@
+/*___________________In the name of Allah________________*/
+/*____________________Solved By shafinkun________________*/
+// Problem link : https://codeforces.com/contest/1996/problem/A
+
+#include <bits/stdc++.h>
+using namespace std;
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define endl '\n'
+#define int long long
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+const int MOD = 1e9 + 7;
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    int cows = n / 4;
+
+    n %= 4;
+
+    int chickens = n / 2;
+
+    cout << cows + chickens << endl;
+
+    return;
+}
+
+signed main()
+{
+    optimize();
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}

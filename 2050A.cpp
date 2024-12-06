e613203059ffac7e8ac1a0b8c39807d62ca07887
shafinkun <sh.shafin20@gmail.com>
2024-12-07 00:05:15 +0600

    Using a single for loop gets WA! IDK why!

---
+++ 2050A.cpp
@@ -0,0 +1,53 @@
+/*___________________In the name of Allah________________*/
+/*____________________Solved By shafinkun________________*/
+// Problem link : https://codeforces.com/contest/2050/problem/A
+// Inqilab Zindabad!!!
+#include <bits/stdc++.h>
+using namespace std;
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define endl '\n'
+#define int long long
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+const int MOD = 1e9 + 7;
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+
+    vector<string> v(n);
+    for (auto &x : v)
+        cin >> x;
+
+    int cnt = 0;
+    for (auto x : v)
+    {
+        if (x.size() <= m)
+        {
+            m -= x.size();
+            cnt++;
+        }
+        else
+            break;
+    }
+
+    cout << cnt << endl;
+
+    return;
+}
+
+signed main()
+{
+    optimize();
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
